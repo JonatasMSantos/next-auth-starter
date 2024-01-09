f80a41e0691fb7568ac79bb474d9601773efe412
Jônatas Santos <jts.msantos@gmail.com>
2024-01-09 17:02:31 -0300

    feat() add auth and register components, midlewares and prisma schema

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/components/auth/login-form";
+
+interface LoginProps {
+  
+}
+
+export default function Login(props: LoginProps) {
+  return (
+    <LoginForm/>
+  )
+}
